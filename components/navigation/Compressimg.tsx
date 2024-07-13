e0e9be7edaf55fbdb252c8de0d1d320279ef043b
ak1419479 <ak1419479@gmail.com>
2024-07-13 20:33:10 +0530

    yy

--- components/navigation/Compressimg.tsx
+++ components/navigation/Compressimg.tsx
@@ -24,68 +24,17 @@ export default function Compressimg() {
     setMyFlag(myFlag +20);
   }
   const [text, setText] = useState('');
-
-
-const NumericTextInput = () => {
-    const [text, setText] = useState('');
-}  
-    const handleTextChange = (inputText) => {
-      // Remove any non-numeric characters using regular expression
-      const numericText = inputText.replace(/[^0-9]/g, '');
-      setText(numericText);
-    };
-    const DocPicker = () => {
-        const [ doc, setDoc ] = useState();
-        const pickDocument = async () => {
-            let result = await DocumentPicker.getDocumentAsync({ type: "*/*", copyToCacheDirectory: true }).then(response => {
-                if (response.type == 'success') {          
-                  let { name, size, uri } = response;
-                  let nameParts = name.split('.');
-                  let fileType = nameParts[nameParts.length - 1];
-                  var fileToUpload = {
-                    name: name,
-                    size: size,
-                    uri: uri,
-                    type: "application/" + fileType
-                  };
-                  console.log(fileToUpload, '...............file')
-                  setDoc(fileToUpload);
-                } 
-              });
-            // console.log(result);
-            console.log("Doc: " + doc.uri);
-        }
-    
-        const postDocument = () => {
-            const url = "http://192.168.10.107:8000/upload";
-            const fileUri = doc.uri;
-            const formData = new FormData();
-            formData.append('document', doc);
-            const options = {
-                method: 'POST',
-                body: formData,
-                headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'multipart/form-data',
-                },
-            };
-            console.log(formData);
-    
-            fetch(url, options).catch((error) => console.log(error));
-        }
-
   return (
     <View style={styles.container}>
       <Text style={{fontSize:20}}> State(Mutable)</Text>
 <Text>My Flag {myFlag}</Text>
-<Button title="Select Document" onPress={pickDocument} />
-            <Button title="Upload" onPress={postDocument} />
+<Button title="Select Document"  />
+            <Button title="Upload"  />
 
 
 <TextInput
         style={styles.input}
         value={text}
-        onChangeText={handleTextChange}
         keyboardType="numeric" // This sets the keyboard to numeric
         placeholder="Enter numeric (KB)"
       />
@@ -102,8 +51,9 @@ const NumericTextInput = () => {
       <StatusBar style="auto" />
     </View>
   );
-}
 
+
+}
 const styles = StyleSheet.create({
     textInput: {
         width: 200,
